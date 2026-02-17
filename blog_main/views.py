3c0d84707f741ec2716b57da5faba5d510a43230
dipalee <109855353+dipspatil20@users.noreply.github.com>
2026-02-17 15:17:27 +0530

    display categories

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from blogs.models import Category, Blog
 
 def home(request):
-    return render(request, 'home.html')
+    categories = Category.objects.all()
+    featured_posts = Blog.objects.filter(is_featured=True, status='published').order_by('updated_at')
+    posts = Blog.objects.filter(is_featured=False, status='published')
+    #print(posts)
+    context = {
+        'categories': categories,
+        'featured_posts': featured_posts,
+        'posts':posts,
+    }
+    return render(request, 'home.html', context)
