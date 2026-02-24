028e7fa76029f0dfe67710002f93ab855d75ca00
dipalee <109855353+dipspatil20@users.noreply.github.com>
2026-02-24 15:55:50 +0530

    posts by category

--- blog_main/views.py
+++ blog_main/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from blogs.models import Category, Blog
 
 def home(request):
-    categories = Category.objects.all()
+    #categories = Category.objects.all()
     featured_posts = Blog.objects.filter(is_featured=True, status='published').order_by('updated_at')
     posts = Blog.objects.filter(is_featured=False, status='published')
     #print(posts)
     context = {
-        'categories': categories,
+        #'categories': categories,
         'featured_posts': featured_posts,
         'posts':posts,
     }
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from . models import Blog, Category
+
 
 # Create your views here.
+def posts_by_category(request, category_id):
+    #print(category_id)
+    
+    #fetch the post thaat belong to category with id which is Category_id
+    posts = Blog.objects.filter(status='published', category_id=category_id)
+    #use try/except when we want to do some cutom action if the categoru does not exists
+    # try:
+    #     category = Category.objects.get(pk=category_id)
+    # except:
+    #     return redirect('home')
+
+    #use get_object_or_404 when you want ro show 404 erroe page if the category does not exist
+    category = get_object_or_404(Category, pk=category_id)
+
+    context = {
+        'posts': posts,
+        'category': category
+    }
+    return render(request, 'posts_by_category.html', context)
